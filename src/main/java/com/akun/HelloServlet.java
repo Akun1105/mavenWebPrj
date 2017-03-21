28089dbd0566eca283bd9533b5fed6ac26949ead
Akun1105 <hu.bing_wk@aliyun.com>
2017-03-21 17:35:32 +0800

    Initial commit

---
+++ src/main/java/com/akun/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.akun;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Akun on 2017/1/25.
+ */
+@WebServlet(urlPatterns={"/HelloServlet"})
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        System.out.println("AAAAAAAAAAAAABBBBBBBBBBBBB");
+        String visitDate = dateFormat.format(new Date());
+        req.setAttribute("visitDate", visitDate);
+        req.getRequestDispatcher("/jsp/hello.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/jsp/hello.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html"
+         pageEncoding="UTF-8"%>
+<html>
+<head>
+    <title>Insert title here</title>
+</head>
+<body>
+<h1>Hello!</h1>
+<h2>访问时间：${visitDate}</h2>
+
+</body>
+</html>
